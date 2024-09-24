f22083fe509851fbcb2fd75f4512afc31fbbe63c
davi.sousa <davi.sousa@placeti.com.br>
2024-09-24 14:27:29 -0300

    tentando criar migration

--- backend/src/main/java/com/bazinga/BazingaApplication.java
+++ backend/src/main/java/com/bazinga/BazingaApplication.java
@@ -2,11 +2,13 @@ package com.bazinga;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.AbstractEnvironment;
 
 @SpringBootApplication
 public class BazingaApplication {
 
 	public static void main(String[] args) {
+		System.setProperty(AbstractEnvironment.ACTIVE_PROFILES_PROPERTY_NAME, "dev");
 		SpringApplication.run(BazingaApplication.class, args);
 	}
 
---
+++ backend/src/main/resources/db/migration/V1__create_schemas.sql
@@ -0,0 +1,2 @@
+create schema if not exists controle;
+create schema if not exists seguranca;
