8225cafbdad5cea931b9cf86a3f99f70512b3545
davizord <crisostomo.davi@gmail.com>
2024-10-23 02:35:25 -0300

    tentando fazer o bagulho do inicio

--- frontend/bazinga/src/app/bazinga/bazinga.module.ts
+++ frontend/bazinga/src/app/bazinga/bazinga.module.ts
@@ -4,6 +4,7 @@ import {HomeComponent} from "./componentes/home/home.component";
 import {InicioComponent} from "./componentes/inicio/inicio.component";
 import {BazingaRoutingModule} from "./bazinga-routing.module";
 import {DefaultNavbarComponent} from "../shared/components/default-menu/default-navbar/default-navbar.component";
+import {FooterComponent} from "../shared/components/footer/footer.component";
 
 
 
@@ -12,7 +13,7 @@ import {DefaultNavbarComponent} from "../shared/components/default-menu/default-
   imports: [
     BazingaRoutingModule,
     CommonModule,
-    DefaultNavbarComponent
-  ]
+    DefaultNavbarComponent,
+    FooterComponent]
 })
 export class BazingaModule { }
