d061ea32525f130460d7e347e40dd3a5e8f0f8d4
davizord <crisostomo.davi@gmail.com>
2024-10-25 21:57:02 -0300

    aaaaaaaaaaaaa

--- frontend/bazinga/src/app/bazinga/componentes/inicio/inicio.component.ts
+++ frontend/bazinga/src/app/bazinga/componentes/inicio/inicio.component.ts
@@ -1,18 +1,36 @@
-import { Component , CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import {AfterViewInit, Component, CUSTOM_ELEMENTS_SCHEMA, OnInit} from '@angular/core';
 import {FooterComponent} from "../../../shared/components/footer/footer.component";
+import {NgClass} from "@angular/common";
+import Swiper from "swiper";
 
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.scss',
   standalone: true,
-  imports:[
-    FooterComponent
+  imports: [
+    FooterComponent,
+    NgClass
   ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 
 
-export class InicioComponent {
+export class InicioComponent implements AfterViewInit {
+  maskApplied = false;
+  filterApplied = false;
+  ngAfterViewInit() {
+    new Swiper('.slider-mask', {
+      autoplay: {
+        delay: 3000,
+      },
+      loop: true,
+    });
 
+    // Aplicando a máscara após um tempo
+    setTimeout(() => {
+      this.maskApplied = true;
+      this.filterApplied = true;
+    }, 3000);
+  }
 }
